91a12d62b8bc527175c119861be3649a0d459215
nprasad488 <nprasad488@gmail.com>
2022-12-01 19:10:51 +0530

    parseed input and output of json paramater

--- src/json/jsonParser.cpp
+++ src/json/jsonParser.cpp
@@ -1,28 +1,50 @@
 #include "../AetherEncoder.h"
+#include "jsonParser.h"
+
+
 using namespace std;
  
  int AetherAction::parseJson(string fpath) 
  {
-    
+    ofstream newJson;
+    struct input in;
+    struct output out;
     //opening file using fstream
     ifstream json(fpath);
  
     // check if there is any error is getting data from the json file
-        if (!reader.parse(json, root)) {
+    if (!reader.parse(json, root)) {
         cout << reader.getFormattedErrorMessages();
         exit(1);
-        }
-       // cout << root << endl; // show the contain of file
-        //Updating the json data
-        cout << root["Input"][0]["type"]["SDI"]["active"] << endl;
+    }
+       
+    in.active_type = root["Input"][0]["active_type"].asString();
+    in.framerate = root["Input"][0]["framerate"].asFloat();
+    in.res.height = root["Input"][0]["resolution"]["height"].asInt();
+    in.res.width = root["Input"][0]["resolution"]["weight"].asInt();
+    in.type.ip.active = root["Input"][0]["type"]["IP"]["active"].asString();
+    in.type.ip.input_type = root["Input"][0]["type"]["IP"]["input_type"].asString();
+    in.type.ip.ipAddr = root["Input"][0]["type"]["IP"]["ipAddr"].asString();
+    in.type.ip.port = root["Input"][0]["type"]["IP"]["port"].asInt();
+    in.type.ip.sys_IP = root["Input"][0]["type"]["IP"]["input_type"].asString();
+
+    out.active_type = root["Input"][0]["active_type"].asString();
+    out.bitrate = root["Input"][0]["bitrate"].asInt();
+    out.encode = root["Input"][0]["encode"].asString();
+    out.framerate = root["Input"][0]["framerate"].asFloat();
+    out.res.height = root["Input"][0]["resolution"]["height"].asInt();
+    out.res.width = root["Input"][0]["resolution"]["weight"].asInt();
+    out.type.ip.active = root["Input"][0]["type"]["IP"]["active"].asString();
+    out.type.ip.input_type = root["Input"][0]["type"]["IP"]["input_type"].asString();
+    out.type.ip.ipAddr = root["Input"][0]["type"]["IP"]["ipAddr"].asString();
+    out.type.ip.port = root["Input"][0]["type"]["IP"]["port"].asInt();
+    out.type.ip.sys_IP = root["Input"][0]["type"]["IP"]["input_type"].asString();
+
+    cout << out.active_type << endl << out.framerate << endl << out.res.height << endl << out.type.ip.active << endl;
  
-        //we can add new values as well
-       root["Input"][0]["type"]["SDI"]["active"] = "no";
-      /*  root["last"] = "Jain";*/
- 
-        // make the changes in a new file/original file
-        newJson.open(fpath);
-        writer.write(newJson, root);
-        newJson.close();
+    //make the changes in a new file/original file
+    //newJson.open(fpath);
+    //writer.write(newJson, root);
+    //newJson.close();
     return 0;
 }
