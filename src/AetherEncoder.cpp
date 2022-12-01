a8446b656af39c9461686e6f19cf5bb0c3fa3c98
nprasad488 <nprasad488@gmail.com>
2022-12-01 19:20:19 +0530

    call parser and pipeline

--- src/AetherEncoder.cpp
+++ src/AetherEncoder.cpp
@@ -1,8 +1,14 @@
 #include "AetherEncoder.h"
+#include "gstreamer/gst.h"
 
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
-    
+    AetherAction AA;
+    gst_init (&argc, &argv);
 
+    AA.parseJson("../sample.json");
+    
+    cout<< "calling gstreamer" << endl;
+    check_gstreamer();
 }
